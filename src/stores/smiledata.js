13f34294dbea7b89c568c3774958e67e1e984e63
emilyliquin <emilyliquin@gmail.com>
2023-02-14 15:36:20 -0500

    Initialize randomization

---
+++ src/stores/smiledata.js
@@ -0,0 +1,200 @@
+import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
+import appconfig from '@/config'
+
+import {
+  createDoc,
+  updateSubjectDataRecord,
+  updateExperimentCounter,
+  loadDoc,
+  fsnow,
+} from './firestore-db'
+
+
+export default defineStore('smilestore', {
+  // arrow function recommended for full type inference
+  state: () => ({
+    local: useStorage(appconfig.local_storage_key, {
+      // syncs with local storage
+      knownUser: false,
+      lastRoute: appconfig.mode === 'development' ? 'recruit' : 'landing',
+      allowJumps: appconfig.mode === 'development',
+      docRef: null,
+      partNum: null,
+      completionCode: '',
+      totalWrites: 0,
+      lastWrite: null,
+      seedActive: true,
+      seedID: '',
+      seedSet: false,
+    }, localStorage, { mergeDefaults: true }),
+    global: {
+      // ephemeral state, resets on browser refresh
+      progress: 0,
+      page_bg_color: '#fff',
+      page_text_color: '#000',
+      status_bar_bg_color: '#fff',
+      status_bar_text_color: '#000',
+      db_connected: false,
+      search_params: null,
+    },
+    dev: {
+      // ephemeral state, utilized by developer mode functions
+      page_provides_autofill: null,
+    },
+    data: {
+      // syncs with firestore
+      trial_num: 0, // not being updated correctly
+      consented: false,
+      done: false,
+      recruitment_service: 'web', // fake
+      recruitment_info: {}, // empty
+      browser_data: [], // empty
+      demographic_form: {}, // empty
+      withdraw: false, // false
+      withdraw_data: {}, // empty
+      route_order: [],
+      conditions: {}
+    },
+    config: appconfig,
+  }),
+
+  getters: {
+    isKnownUser: (state) => state.local.knownUser,
+    isConsented: (state) => state.data.consented,
+    isDone: (state) => state.data.done,
+    lastRoute: (state) => state.local.lastRoute,
+    isDBConnected: (state) => state.global.db_connected,
+    hasAutofill: (state) => state.dev.page_provides_autofill,
+    searchParams: (state) => state.global.search_params,
+    recruitmentService: (state) => state.data.recruitment_service,
+    isSeedSet: (state) => state.local.seedSet,
+    getSeedID: (state) => state.local.seedID
+  },
+
+  actions: {
+    setDBConnected() {
+      this.global.db_connected = true
+    },
+    setSearchParams(search_params) {
+      this.global.search_params = search_params
+    },
+    setConsented() {
+      this.data.consented = true
+    },
+    setDone() {
+      this.data.done = true
+    },
+    setCompletionCode(code) {
+      this.local.completionCode = code
+    },
+    setSeedID(seed) {
+      this.local.seedID = seed
+      this.local.seedSet = true
+    },
+    recordWindowEvent(type, event_data = null) {
+      if (event_data) {
+        this.data.browser_data.push({
+          event_type: type,
+          timestamp: fsnow(),
+          event_data,
+        })
+      } else {
+        this.data.browser_data.push({
+          event_type: type,
+          timestamp: fsnow(),
+        })
+      }
+    },
+    setPageAutofill(fn) {
+      this.dev.page_provides_autofill = fn
+    },
+    removePageAutofill() {
+      this.dev.page_provides_autofill = null
+    },
+    setRecruitmentService(service, info) {
+      this.data.recruitment_service = service
+      this.data.recruitment_info = info
+    },
+    autofill() {
+      if (this.dev.page_provides_autofill) {
+        this.dev.page_provides_autofill()
+      }
+    },
+    saveDemographicForm(data) {
+      this.data.demographic_form = data
+    },
+    setCondition(name, cond) {
+      this.data.conditions[name] = cond
+    },
+    async setKnown() {
+      this.local.knownUser = true
+      this.local.partNum = await updateExperimentCounter('participants')
+      this.local.docRef = await createDoc(this.data, this.local.seedID, this.local.partNum)
+
+      // assign conditions, with id number for randomization
+      // this.assignConds(this.local.partNum)
+
+      if (this.local.docRef) {
+        this.setDBConnected()
+      }
+    },
+    async loadData() {
+      let data
+      if (this.local.docRef) {
+        data = await loadDoc(this.local.docRef)
+      }
+      if (data) {
+        this.data = data
+        this.setDBConnected()
+      }
+    },
+    setLastRoute(route) {
+      this.local.lastRoute = route
+      // if (route !== 'config') {
+      //   this.local.lastRoute = route
+      // }
+    },
+    recordRoute(route){
+      this.data.route_order.push(route)
+    },
+    async saveData(force = false) {
+      if (this.isDBConnected) {
+        if (!force && this.local.totalWrites >= appconfig.max_writes) {
+          console.error(
+            'SMILESTORE: max writes reached to firebase.  Data NOT saved.  Call saveData() less numerously to avoid problems/cost issues.'
+          )
+          return
+        }
+
+        if (
+          !force &&
+          this.local.lastWrite &&
+          Date.now() - this.local.lastWrite < appconfig.min_write_interval
+        ) {
+          console.error(
+            'SMILESTORE: write interval too short for firebase.  Data NOT saved. Call saveData() less frequently to avoid problems/cost issues.'
+          )
+          console.error(
+            'SMILESTORE: interval was',
+            appconfig.min_write_interval
+          )
+          // console.error(Date.now() - this.local.lastWrite)
+          return
+        }
+        updateSubjectDataRecord(this.data, this.local.docRef)
+        this.local.totalWrites += 1
+        this.local.lastWrite = Date.now()
+        console.log('Request to firebase successful')
+      }
+    },
+    resetLocal() {
+      // this.local.knownUser = false
+      // this.local.lastRoute = 'welcome'
+      // this.local.allowJumps = false
+      // this.global.db_connected = false
+      this.$reset()
+    },
+  },
+})
+
---
+++ src/timeline.js
@@ -0,0 +1,211 @@
+import _ from 'lodash'
+import seedrandom from 'seedrandom'
+import * as random from '@/randomization'
+import { pinia } from '@/createpinia'
+import '@/seed'
+import useSmileStore from '@/stores/smiledata' // get access to the global store
+
+
+class Timeline {
+  constructor() {
+    this.routes = [] // the actual routes given to VueRouter
+    this.seqtimeline = [] // copies of routes that are sequential
+    this.randroutes = [] // temporary holder used for route randomization
+  }
+
+  pushToRoutes(route) {
+    // check that an existing route doesn't exist with same
+    // path and/or name
+    for (let i = 0; i < this.routes.length; i += 1) {
+      if (this.routes[i].path === route.path) {
+        throw new Error(`DuplicatePathError:${route.path}`)
+      }
+      if (this.routes[i].name === route.name) {
+        throw new Error(`DuplicateNameError:${route.name}`)
+      }
+    }
+    this.routes.push(route)
+  }
+
+  pushToRandRoutes(route) {
+    // check that an existing route doesn't exist with same
+    // path and/or name
+    for (let i = 0; i < this.randroutes.length; i += 1) {
+      if (this.randroutes[i].path === route.path) {
+        throw new Error(`DuplicatePathError:${route.path}`)
+      }
+      if (this.randroutes[i].name === route.name) {
+        throw new Error(`DuplicateNameError:${route.name}`)
+      }
+    }
+    this.randroutes.push(route)
+  }
+
+  pushToTimeline(route) {
+    // check that an existing route doesn't exist with same
+    // path and/or name
+    for (let i = 0; i < this.seqtimeline.length; i += 1) {
+      if (this.seqtimeline[i].path === route.path) {
+        throw new Error(`DuplicatePathError${route.path}`)
+      }
+      if (this.seqtimeline[i].name === route.name) {
+        throw new Error(`DuplicateNameError${route.name}`)
+      }
+    }
+    this.seqtimeline.push(route)
+  }
+
+  pushSeqRoute(routeConfig) {
+    const newroute = _.cloneDeep(routeConfig)
+    if (!newroute.meta) {
+      newroute.meta = { next: undefined, prev: undefined } // need to configure it
+    } else {
+      if (!newroute.meta.next) {
+        // need to configure next
+        newroute.meta.next = undefined
+      }
+
+      if (!newroute.meta.prev) {
+        // need to configure prev
+        newroute.meta.prev = undefined
+      }
+    }
+    newroute.meta.sequential = true
+
+    try {
+      this.pushToRoutes(newroute)
+    } catch (err) {
+      console.error('Smile FATAL ERROR: ', err)
+      throw err
+    }
+
+    try {
+      this.pushToTimeline(newroute) // by reference so should update together
+    } catch (err) {
+      console.error('Smile FATAL ERROR: ', err)
+      throw err
+    }
+  }
+
+  pushRandRoute(routeConfig) {
+    const newroute = _.cloneDeep(routeConfig)
+    if (!newroute.meta) {
+      newroute.meta = { next: undefined, prev: undefined } // need to configure it
+    } else {
+      if (!newroute.meta.next) {
+        // need to configure next
+        newroute.meta.next = undefined
+      }
+
+      if (!newroute.meta.prev) {
+        // need to configure prev
+        newroute.meta.prev = undefined
+      }
+    }
+    newroute.meta.sequential = true
+
+    try {
+      this.pushToRandRoutes(newroute) // by reference so should update together
+    } catch (err) {
+      console.error('Smile FATAL ERROR: ', err)
+      throw err
+    }
+  }
+
+  resolveRandRoutes(key) {
+    const smilestore = useSmileStore(pinia)
+
+    // set seed from seed_id, which is in local storage
+    const seedID = smilestore.getSeedID
+    seedrandom(`${seedID}-${key}`, { global: true });
+
+    // randomly shuffle the routes in randroutes
+    this.randroutes = random.shuffle(this.randroutes);
+
+    Object.values(this.randroutes).forEach(route => {
+      if(route.meta.rand !== key){ // check if route matches key
+        console.error(`random route ${  route.name  } doesn't have meta field matching resolution key -- possible error`)
+      }
+      const newroute = _.cloneDeep(route)
+
+      newroute.meta.sequential = true
+
+      try {
+        this.pushToRoutes(newroute) // add to routes list
+      } catch (err) {
+        console.error('Smile FATAL ERROR: ', err)
+        throw err
+      }
+      try {
+        this.pushToTimeline(newroute) // add to timeline
+      } catch (err) {
+        console.error('Smile FATAL ERROR: ', err)
+        throw err
+      }
+    });
+
+    // remove random routes so that you can reuse this later
+    this.randroutes = []
+  }
+
+  pushRoute(routeConfig) {
+    const newroute = _.cloneDeep(routeConfig)
+    // should NOT allow meta next/prev to exist
+    if (!newroute.meta) {
+      newroute.meta = { prev: null, next: null }
+    } else if (newroute.meta.prev || newroute.meta.next) {
+      throw new Error(
+        `NonSequentialRouteError: Can't have meta.next or meta.prev defined for non-sequential route`
+      )
+    }
+    newroute.meta.sequential = false
+    try {
+      this.pushToRoutes(newroute)
+    } catch (err) {
+      console.error('Smile FATAL ERROR: ', err)
+      throw err
+    }
+  }
+
+  build() {
+    this.buildGraph()
+    // this.buildProgress()
+  }
+
+  // buildGraph builds
+  buildGraph() {
+    for (let i = 0; i < this.seqtimeline.length; i += 1) {
+      if (this.seqtimeline[i].meta.next === undefined) {
+        // pass
+        if (i === 0) {
+          this.seqtimeline[i].meta.next = this.seqtimeline[i + 1].name
+        } else if (i === this.seqtimeline.length - 1) {
+          this.seqtimeline[i].meta.next = null
+        } else {
+          this.seqtimeline[i].meta.next = this.seqtimeline[i + 1].name
+        }
+      }
+      if (this.seqtimeline[i].meta.prev === undefined) {
+        // pass
+        if (i === 0) {
+          this.seqtimeline[i].meta.prev = null
+        } else if (i === this.seqtimeline.length - 1) {
+          this.seqtimeline[i].meta.prev = this.seqtimeline[i - 1].name
+        } else {
+          this.seqtimeline[i].meta.prev = this.seqtimeline[i - 1].name
+        }
+      }
+    }
+  }
+
+  // this won't work with new system
+  // buildProgress assigns progrees meeter values to each route
+  // buildProgress() {
+  //   const seqTimelineLength = this.seqtimeline.length
+  //   for (let i = 0; i < seqTimelineLength; i++) {
+  //     this.seqtimeline[i].meta.routeIdx = i
+  //     this.seqtimeline[i].meta.progress = (100 * i) / (seqTimelineLength - 1)
+  //   }
+  // }
+}
+export default Timeline
