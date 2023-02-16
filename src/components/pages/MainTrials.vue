fc91f1b5dc13945e21d338797ecc8addecc88a39
emilyliquin <emilyliquin@gmail.com>
2023-02-16 08:03:22 -0500

    randomize trials, fix deploy?

--- src/components/pages/MainTrials.vue
+++ src/components/pages/MainTrials.vue
@@ -22,28 +22,42 @@ const { next, prev } = useTimelineStepper()
 if(route.meta.progress) smilestore.global.progress = route.meta.progress
 
 
-const pages = [ 
+const pages1 = [
 {comp: VidAutoAdvance, args:{id: 1, vid_name: "trial_mixing"}},
 {comp: VidClickArrow, args:{id: 2, vid_name: "mt1_1"}},
 {comp: VidAutoAdvance, args:{id: 3, vid_name: "mt1_2"}},
+]
+const pages2 = [
 {comp: VidAutoAdvance, args:{id: 4, vid_name: "trial_mixing"}},
 {comp: VidClickArrow, args:{id: 5, vid_name: "mt2_1"}},
 {comp: VidAutoAdvance, args:{id: 6, vid_name: "mt2_2"}},
+]
+const pages3 = [
 {comp: VidAutoAdvance, args:{id: 7, vid_name: "trial_mixing"}},
 {comp: VidClickArrow, args:{id: 8, vid_name: "mt3_1"}},
 {comp: VidAutoAdvance, args:{id: 9, vid_name: "mt3_2"}},
-{comp: VidClickArrow, args:{id: 10, vid_name: "interim_6"}},
-{comp: VidAutoAdvance, args:{id: 11, vid_name: "trial_mixing"}},
-{comp: VidClickArrow, args:{id: 12, vid_name: "mt4_1"}},
-{comp: VidAutoAdvance, args:{id: 13, vid_name: "mt4_2"}},
-{comp: VidAutoAdvance, args:{id: 14, vid_name: "trial_mixing"}},
-{comp: VidClickArrow, args:{id: 15, vid_name: "mt5_1"}},
-{comp: VidAutoAdvance, args:{id: 16, vid_name: "mt5_2"}},
-{comp: VidAutoAdvance, args:{id: 17, vid_name: "trial_mixing"}},
-{comp: VidClickArrow, args:{id: 18, vid_name: "mt6_1"}},
-{comp: VidAutoAdvance, args:{id: 19, vid_name: "mt6_2"}},
-{comp: VidAutoAdvance, args:{id: 20, vid_name: "end"}},
 ]
+const pages4 = [
+{comp: VidAutoAdvance, args:{id: 10, vid_name: "trial_mixing"}},
+{comp: VidClickArrow, args:{id: 11, vid_name: "mt4_1"}},
+{comp: VidAutoAdvance, args:{id: 12, vid_name: "mt4_2"}},
+]
+const pages5 = [
+{comp: VidAutoAdvance, args:{id: 13, vid_name: "trial_mixing"}},
+{comp: VidClickArrow, args:{id: 14, vid_name: "mt5_1"}},
+{comp: VidAutoAdvance, args:{id: 15, vid_name: "mt5_2"}},
+]
+const pages6 = [
+{comp: VidAutoAdvance, args:{id: 16, vid_name: "trial_mixing"}},
+{comp: VidClickArrow, args:{id: 17, vid_name: "mt6_1"}},
+{comp: VidAutoAdvance, args:{id: 18, vid_name: "mt6_2"}},
+]
+
+const pagesNested = [pages1, pages2, pages3, pages4, pages5, pages6]
+const pagesShuffled = random.shuffle(pagesNested)
+
+let pages = pagesShuffled.flat()
+pages = pages.splice(9, 0, {comp: VidClickArrow, args:{id: 10, vid_name: "interim_6"}});
 
 let page_indx = 0
 
