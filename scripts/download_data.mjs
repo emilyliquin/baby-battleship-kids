220000a01de90ce4f6759fc4f19da116d38c12b4
emilyliquin <emilyliquin@gmail.com>
2023-03-14 15:41:03 -0400

    update download data

--- scripts/download_data.mjs
+++ scripts/download_data.mjs
@@ -4,10 +4,12 @@
 import inquirer from 'inquirer'
 import chalk from 'chalk'
 import figlet from 'figlet'
+import { execSync } from 'child_process'
 // import shell from 'shelljs'
-import { initializeApp } from 'firebase/app'
+// import { initializeApp } from 'firebase/app'
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore } from "firebase-admin/firestore"
 import {
-  getFirestore,
   collection,
   getDocs,
   query,
@@ -40,15 +42,29 @@ const askQuestions = () => {
     },
     {
       type: 'list',
-      name: 'COMPLETE_ONLY',
-      message: 'Do you want all the data or just that was marked complete?',
-      choices: ['all', 'complete_only'],
+      name: 'CLEANUP',
+      message: 'What do you want to do with any temporary data? (WARNING: only select "save to permanent" if there are no in-progress sessions)',
+      choices: ['leave it alone', 'save to permanent'],
     },
+    {
+      type: 'list',
+      name: 'COMPLETE_ONLY',
+      message: 'Do you want all the data, or just the data that was marked done?',
+      choices: ['all', 'done_only'],
+    },
+
     {
       type: 'input',
       name: 'FILENAME',
       message: 'What is the name of the file without extension?',
       default: 'data',
+    },
+
+    {
+      type: 'input',
+      name: 'KEY_PATH',
+      message: 'What is the path to your service account key?',
+      default:'',
     },
   ]
   return inquirer.prompt(questions)
@@ -62,7 +78,7 @@ const storeData = async (data, path) => {
   }
 }
 
-const getData = async (path, completeOnly, filename) => {
+const connectFirestore = (certPath) => {
   const localenv = dotenv.config({ path: 'env/.env.local' })
   const firebaseConfig = {
     apiKey: localenv.parsed.VITE_FIREBASE_APIKEY,
@@ -71,16 +87,20 @@ const getData = async (path, completeOnly, filename) => {
     storageBucket: localenv.parsed.VITE_FIREBASE_STORAGEBUCKET,
     messagingSenderId: localenv.parsed.VITE_FIREBASE_MESSAGINGSENDERID,
     appId: localenv.parsed.VITE_FIREBASE_APPID,
+    credential: cert(certPath)
   }
   const app = initializeApp(firebaseConfig)
   const db = getFirestore(app)
+  return db
+}
+
+const getData = async (path, completeOnly, db, filename) => {
   let querySnapshot = null
 
   if (completeOnly == 'all') {
-    querySnapshot = await getDocs(collection(db, path))
+    querySnapshot = await db.collection(path).get()
   } else {
-    const q = query(collection(db, path), where('complete', '==', true))
-    querySnapshot = await getDocs(q)
+    querySnapshot = await db.collection(path).where("done", "==", true).get();
   }
   const data = []
   querySnapshot.forEach((doc) => {
@@ -96,6 +116,47 @@ const success = (filename) => {
   )
 }
 
+
+const cleanUpData = async (mode, studyID, db) => {
+
+  const tempDataCollection = db.collection(mode).doc(studyID).collection("temp");
+  const permanentDataCollection = db.collection(mode).doc(studyID).collection("data");
+
+  // get each document in temporary data storage, add bonus, then move to permanent data storage
+  const querySnapshot = await tempDataCollection.get()
+  querySnapshot.forEach(async (doc) => {
+    const docRef = doc.id;
+    let docData = doc.data();
+
+    // if they finished the study, compute the bonus
+    if (docData.done === true) {
+      // ADD CODE HERE TO COMPUTE BONUS
+      const participantBonus = 0;
+      // then save the bonus to the data
+      docData.bonus = participantBonus;
+    }
+    
+    // Get the private data
+    const tempPrivate = tempDataCollection.doc(docRef).collection("private").doc("private_data");
+    const privatedoc = await tempPrivate.get()
+    // then save it to the data as well
+    if (privatedoc.exists) {
+      docData = Object.assign(docData, privatedoc.data());
+    }
+    // save the data to the permanent location
+    const batch = db.batch();
+    await batch.set(permanentDataCollection.doc(docRef), docData);
+    batch.delete(tempPrivate);
+    batch.delete(tempDataCollection.doc(docRef));
+    batch.commit().then(() =>{
+      return null;
+    });
+
+  });
+  return null;
+}
+
+
 const run = async () => {
   // show script introduction
   init()
@@ -104,11 +165,31 @@ const run = async () => {
 
   // ask questions
   const answers = await askQuestions()
-  const { TYPE, COMPLETE_ONLY, FILENAME } = answers
+  const { TYPE, CLEANUP, COMPLETE_ONLY, FILENAME, KEY_PATH } = answers
+
+  // connect to database
+  const localenv = dotenv.config({ path: 'env/.env.local' })
+  const firebaseConfig = {
+    apiKey: localenv.parsed.VITE_FIREBASE_APIKEY,
+    authDomain: localenv.parsed.VITE_FIREBASE_AUTHDOMAIN,
+    projectId: localenv.parsed.VITE_FIREBASE_PROJECTID,
+    storageBucket: localenv.parsed.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: localenv.parsed.VITE_FIREBASE_MESSAGINGSENDERID,
+    appId: localenv.parsed.VITE_FIREBASE_APPID,
+    credential: cert(KEY_PATH)
+  }
+  const app = initializeApp(firebaseConfig)
+  const db = getFirestore(app)
+
+
+  // handle data cleanup
+  if(CLEANUP === "save to permanent"){
+    await cleanUpData(TYPE, project_ref, db)
+  }
 
   // create the file
   const path = `${TYPE}/${project_ref}/data`
-  await getData(path, COMPLETE_ONLY, `${TYPE}-${COMPLETE_ONLY}-${FILENAME}`)
+  await getData(path, COMPLETE_ONLY, db, `${TYPE}-${COMPLETE_ONLY}-${FILENAME}`)
 
   // show success message
   success(`${TYPE}-${COMPLETE_ONLY}-${FILENAME}`)
